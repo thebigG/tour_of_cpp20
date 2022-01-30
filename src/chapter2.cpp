7f1d0ad39f88dd34976ea4743599638435845762
thebigg <yunior.eury@gmail.com>
2022-01-30 17:24:34 -0600

    -Basic chapter 2 example(Variant).

---
+++ src/chapter2.cpp
@@ -0,0 +1,28 @@
+#include "chapter2.h"
+
+std::basic_ostream<char>& operator<<(std::basic_ostream<char, std::char_traits<char>>& __out, const Object& __s)
+{
+   std::string value{std::to_string(__s.objId)};
+   __out.write(value.c_str(), value.size());
+   return __out;
+}
+
+Object::Object(int newId): objId{newId}
+{
+}
+
+VarObject::VarObject(int newVarVal): var{newVarVal}
+{
+
+}
+
+VarObject::VarObject(Object newVarVal): var{newVarVal}
+{
+
+}
+
+Object::Object(Object& newObj): objId{newObj.objId}
+{
+
+}
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
-
-using namespace std;
+#include <variant>
+#include <string>
+#include <utility>
+#include "chapter2.h"
 
 int main()
 {
-	cout << "Hello World!" << endl;
+	// Chapter 2 tests
+	Object a{100};
+	VarObject obj{a};
+	std::cout << "Hello World!" << std::endl;
+	std::cout<<"Value of obj1:" << std::get<Object>(obj.var) <<std::endl;
+	std::cout<<"Value of obj2:" << a <<std::endl;
 	return 0;
 }
