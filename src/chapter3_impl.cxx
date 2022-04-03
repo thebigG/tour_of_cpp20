985791d615408e7a7be0a72bead2f87def6ffc5c
thebigg <yunior.eury@gmail.com>
2022-04-02 19:13:47 -0500

    -Add fmt.
    (cherry picked from commit dfd8b93075c48abccdcf83cdf8bd1021a9b7e70d)

--- src/chapter3_impl.cxx
+++ src/chapter3_impl.cxx
@@ -2,7 +2,6 @@
 
 #include "chapter3.h"
 
-void func() { std::cout << "Modules in C++20" << std::endl; }
 const char* OutOfRangeException::what() const throw() {
   return "Index is out of range.";
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,35 +5,30 @@
 #include <vector>
 
 #include "chapter2.h"
+#include "chapter3.h"
+#include "chapter4.h"
 
 int main() {
   // Chapter 2 tests
   Object a{200};
   VarObject obj{a};
+  Vector_CPP20<double> items{4,  8, 16, 4,  8, 16, 4,  8,
+                             16, 4, 8,  16, 4, 8,  16, 7};
 
-  //  func();
-  //  Vector_CPP20<double> items{4,  8, 16, 4,  8, 16, 4,  8,
-  //                             16, 4, 8,  16, 4, 8,  16, 7};
+  // items.pushBack(15.1);
+  std::cout << "front of vector:" << items.front() << std::endl;
 
-  //  // items.pushBack(15.1);
-  //  std::cout << "front of vector:" << items.front() << std::endl;
+  if (std::holds_alternative<Object>(obj.var)) {
+    std::cout << "Value of obj0:" << std::get<Object>(obj.var) << std::endl;
+  }
 
-  //  if (std::holds_alternative<Object>(obj.var)) {
-  //    std::cout << "Value of obj0:" << std::get<Object>(obj.var) << std::endl;
-  //  }
+  else if (std::holds_alternative<int>(obj.var)) {
+    std::cout << "Value of obj1:" << std::get<int>(obj.var) << std::endl;
+  }
 
-  //  else if (std::holds_alternative<int>(obj.var)) {
-  //    std::cout << "Value of obj1:" << std::get<int>(obj.var) << std::endl;
-  //  }
-
-  //  std::visit(
-  //      [](auto&& var) { std::cout << "visit variant:" << var << std::endl; },
-  //      obj.var);
-  //  std::cout << "Value of obj2:" << a << std::endl;
-
-  //  std::variant<int, std::string> var = "Var";
-
-  std::string str{"Hello"};
-  std::cout << str << std::endl;
+  std::visit(
+      [](auto&& var) { std::cout << "visit variant:" << var << std::endl; },
+      obj.var);
+  std::cout << "Value of obj2:" << a << std::endl;
   return 0;
 }
