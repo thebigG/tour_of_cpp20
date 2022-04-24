4cfe2fdcc4120bfae25484155a998e8d452710ba
thebigg <yunior.eury@gmail.com>
2022-04-24 16:11:01 -0500

    -Ignore chapter 3 from in test coverage.

--- generate_coverage.sh
+++ generate_coverage.sh
@@ -5,5 +5,6 @@ lcov -c -i -b modules/ -d modules/ -o Coverage.baseline
 lcov -c -d . -b modules -o Coverage.out
 lcov -a Coverage.baseline -a Coverage.out -o Coverage.combined
 lcov -r Coverage.combined /usr/include/\* -o Coverage.combined
-lcov -r Coverage.combined *main.cpp* -o Coverage.combined
+lcov -r Coverage.combined "*main.cpp*" -o Coverage.combined
+lcov -r Coverage.combined "*chapter3.h*" -o Coverage.combined
 genhtml Coverage.combined -o HTML
